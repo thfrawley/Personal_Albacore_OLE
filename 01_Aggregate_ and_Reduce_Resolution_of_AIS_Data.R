7253e9487ded59ef91c1202135e53fcb47b87c30
thfrawley <timothyh.frawley@gmail.com>
2020-04-11 13:23:42 -0700

    Added method for mmsi

--- 01_Aggregate_ and_Reduce_Resolution_of_AIS_Data.R
+++ 01_Aggregate_ and_Reduce_Resolution_of_AIS_Data.R
@@ -13,19 +13,19 @@ library(maptools)
 library(rgeos)
 library(ggplot2)
 
-###This should be the directory where files downloaded from the GFW website are located
-data_dir <- 'C:/Users/timot/Documents/GFw_Data/data/fishing_effort/'
+###This should be the directory where the daily files downloaded from the GFW website are located
+data_dir <- 'C:/Users/timot/Documents/Albacore_OLE/GFW_data/raw_data/fishing_effort/'
 
 effort_files <- tibble(
-  file = list.files(paste0(data_dir, 'fishing_effort_byflag'), 
+  file = list.files(paste0(data_dir, 'daily_fishing_effort_bymmsi'), 
                     pattern = '.csv', recursive = T, full.names = T),
   date = ymd(str_extract(file, 
                          pattern = '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}')))
 
-### At native resolution these are very large files; had to read in and convert the data 
+### At 0.01 degree resolution these are very large files; had to read in and convert the data 
 ### One year at a time 2012-2015 and then split 2016 in 6 month intervals
 
-effort_dates <- seq(ymd('2015-01-01'), ymd('2015-12-31'), by='days')
+effort_dates <- seq(ymd('2012-01-01'), ymd('2016-12-31'), by='days')
 
 # Filter to files within our date range of interest
 effort_files <- filter(effort_files, date %in% effort_dates)
@@ -41,9 +41,10 @@ effort_df <- effort_df %>%
 
 
 # Specify new (lower) resolution in degrees for aggregating data
-res <- .25
+res <- 1
 
-# Transform data across all fleets and geartypes
+##METHOD 1
+##Transform data across all fleets and geartypes; use for fishing effort by flag
 effort_df <- effort_df %>% 
   mutate(
     # convert from hundreths of a degree to degrees
@@ -60,9 +61,25 @@ effort_df <- effort_df %>%
             fishing_hours = sum(fishing_hours, na.rm = T),
             mmsi_present  = sum(mmsi_present, na.rm = T))
 
-write.csv(effort_df, "2015_fishingeffortflag_0.25.csv")
+write.csv(effort_df, "X.csv")
 
+## METHOD 2
+## Transform data across all fleets and geartypes; use for fishing effort by mmsi
+effort_df <- effort_df %>% 
+  mutate(
+    # convert from hundreths of a degree to degrees
+    lat_bin = lat_bin / 10, 
+    lon_bin = lon_bin / 10,
+    # calculate new lat lon bins with desired resolution
+    lat_bin = floor(lat_bin/res) * res + 0.5 * res, 
+    lon_bin = floor(lon_bin/res) * res + 0.5 * res)
 
+# Re-aggregate the data to 0.25 degrees
+effort_df <- effort_df %>% 
+  group_by(date, year, month, lon_bin, lat_bin, mmsi) %>% 
+  summarize(fishing_hours = sum(fishing_hours, na.rm = T))
+          
+write.csv(effort_df, "Daily_2012-2016_fishingeffortmmsi_1.0.csv")
 
 
 

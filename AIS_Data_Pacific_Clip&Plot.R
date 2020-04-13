18a580802a1e195a7c16bb61f4c3dc4a8e02261c
thfrawley <timothyh.frawley@gmail.com>
2020-04-13 16:39:40 -0700

    Tweaked mmsi function

--- AIS_Data_Pacific_Clip&Plot.R
+++ AIS_Data_Pacific_Clip&Plot.R
@@ -8,9 +8,9 @@ setwd("C:/Users/timot/Documents/Albacore_OLE")
 
 ###Prepare shapefiles to use in clipping and plotting
 
-Map<-read_sf('C:/Users/timot/Documents/Albacore_OLE/shapefiles/Pacific_Landmasses.shp')
-IATTC<-read_sf("C:/Users/timot/Documents/Albacore_OLE/shapefiles/RFMO_Boundaries/iattc.shp")
-WCPFC<-read_sf('C:/Users/timot/Documents/Albacore_OLE/shapefiles/RFMO_Boundaries/WCPFC.shp')
+Map<-read_sf('shapefiles/Pacific_Landmasses.shp')
+IATTC<-read_sf("shapefiles/RFMO_Boundaries/iattc.shp")
+WCPFC<-read_sf('shapefiles/RFMO_Boundaries/WCPFC.shp')
 
 Pacific_IATTC<-st_shift_longitude(IATTC)
 Pacific_WCPFC<-st_shift_longitude(WCPFC)
@@ -27,25 +27,27 @@ ggplot(Convention_Areas) + geom_sf()
 ggplot(Combined_Convention_Bounds) + geom_sf()
 ggplot(Pacific_Combined_Convention_Bounds) + geom_sf()
 
+
 ### Read in data
 ###Input is GFW global data aggregated at either 1.0 or 0.25 resolution
 
-Fishing_Data<-read.csv("C:/Users/timot/Documents/Albacore_OLE/GFW_Data/processed_data/global/2012-2016_fishingeffortflag_1.0.csv")
+Fishing_Data<-read.csv("GFW_Data/processed_data/global/Daily_2012-2016_fishingeffortmmsi_1.0.csv")
 myvars <- c("date", "year", "month", "lon_bin", "lat_bin", "flag", "geartype", "vessel_hours", "fishing_hours", "mmsi_present")
+myvars <- c("date", "year", "month", "lon_bin", "lat_bin", "mmsi", "fishing_hours")
 Fishing_Data<-Fishing_Data[myvars]
 
 ###Clip data to study area extent and re-center on the Pacific
 ###METHOD 1
-All_Spatial_Fishing_Data <- st_as_sf(Fishing_Data, coords = c('lon_bin', 'lat_bin'), crs = "+init=epsg:4326")
-Pacific_Spatial_Fishing_Data<-st_intersection(All_Spatial_Fishing_Data, Combined_Convention_Bounds)
-Pacific_Spatial_Fishing_Data<-st_shift_longitude(Pacific_Spatial_Fishing_Data) 
-Lon_Lat_Coords<-as.data.frame(st_coordinates(Pacific_Spatial_Fishing_Data))
+Fishing_Data <- st_as_sf(Fishing_Data, coords = c('lon_bin', 'lat_bin'), crs = "+init=epsg:4326")
+Fishing_Data<-st_intersection(Fishing_Data, Combined_Convention_Bounds)
+Fishing_Data<-st_shift_longitude(Fishing_Data) 
+Lon_Lat_Coords<-as.data.frame(st_coordinates(Fishing_Data))
 names(Lon_Lat_Coords)<-c("Lon","Lat")
-Pacific_Fishing_df<-as.data.frame(Pacific_Spatial_Fishing_Data)
-Pacific_Fishing_df<-cbind(Pacific_Fishing_df, Lon_Lat_Coords)
-Pacific_Fishing_df<- Pacific_Fishing_df[c(-9,-10)]
+Fishing_Data<-as.data.frame(Fishing_Data)
+Fishing_Data<-cbind(Fishing_Data, Lon_Lat_Coords)
+Fishing_Data<- Fishing_Data[c(-9,-10)]  ### Change to -6,-7 for mmsi
 
-write.csv(Pacific_Fishing_df, "XXX.csv")
+write.csv(Fishing_Data, "XXX.csv")
 
 ###METHOD 2
 ###Helps with memory limited computers
@@ -58,30 +60,30 @@ Complete_Clip<-NULL
 
 for (i in 1:length(List_Month)){
   month = List_Month[i]
-  Single_Month <- Fishing_Data[which(Fishing_Data$month == month),]
-  All_Spatial_Fishing_Data <- st_as_sf(Single_Month, coords = c('lon_bin', 'lat_bin'), crs = "+init=epsg:4326")
-  Pacific_Spatial_Fishing_Data<-st_intersection(All_Spatial_Fishing_Data, Combined_Convention_Bounds)
-  Pacific_Spatial_Fishing_Data<-st_shift_longitude(Pacific_Spatial_Fishing_Data) 
-  Lon_Lat_Coords<-as.data.frame(st_coordinates(Pacific_Spatial_Fishing_Data))
+  Fishing_Data_Subset <- Fishing_Data[which(Fishing_Data$month == month),]
+  Fishing_Data_Subset <- st_as_sf(Fishing_Data_Subset, coords = c('lon_bin', 'lat_bin'), crs = "+init=epsg:4326")
+  Fishing_Data_Subset <-st_intersection(Fishing_Data_Subset, Combined_Convention_Bounds)
+  Fishing_Data_Subset <-st_shift_longitude(Fishing_Data_Subset) 
+  Lon_Lat_Coords<-as.data.frame(st_coordinates(Fishing_Data_Subset))
   names(Lon_Lat_Coords)<-c("Lon","Lat")
-  Pacific_Fishing_df<-as.data.frame(Pacific_Spatial_Fishing_Data)
-  Pacific_Fishing_df<-cbind(Pacific_Fishing_df, Lon_Lat_Coords)
-  Pacific_Fishing_df<- Pacific_Fishing_df[c(-9,-10)]
-  Complete_Clip<-rbind(Complete_Clip, Pacific_Fishing_df)
+  Fishing_Data_Subset <-as.data.frame(Fishing_Data_Subset)
+  Fishing_Data_Subset <-cbind(Fishing_Data_Subset, Lon_Lat_Coords)
+  Fishing_Data_Subset <-Fishing_Data_Subset [c(-6,-7)]  ### Change to -6,-7 for mmsi, -9,-10 for byflag
+  Complete_Clip<-rbind(Complete_Clip, Fishing_Data_Subset)
   print(Sys.time())
 }
 
-write.csv(Complete_Clip, "X")
+write.csv(Complete_Clip, "GFW_Data/processed_data/pacific/Daily_Pacific_2012_2016_fishingeffortmmsi_1.0.csv")
 
 ###Aggregate By Grid Cell
 
-effort_all <- Second %>% 
+effort_all <- Complete_Clip %>% 
   group_by(Lon,Lat) %>% 
   summarize(fishing_hours = sum(fishing_hours, na.rm = T),
             log_fishing_hours = log10(sum(fishing_hours, na.rm = T))) %>% 
   ungroup() %>%
   mutate(log_fishing_hours = ifelse(log_fishing_hours <= 1, 1, log_fishing_hours),
-         log_fishing_hours = ifelse(log_fishing_hours >= 5, 5, log_fishing_hours)) %>% 
+         log_fishing_hours = ifelse(log_fishing_hours >= 10, 10, log_fishing_hours)) %>% 
   filter(fishing_hours >= 24)
 
 
@@ -103,7 +105,7 @@ p1 <- effort_all %>%
     limits = c(1, 5),
     colours = effort_pal(5), # Linear Green
     labels = c("10", "100", "1,000", "10,000", "100,000+"),
-    values = scales::rescale(c(0, 1))) +
+    values = scales::rescale(c(0, 1)))+
   labs(fill  = 'Fishing hours (log scale)',
        title = 'Global fishing effort in 2016') +
   guides(fill = guide_colourbar(barwidth = 10))
@@ -144,7 +146,7 @@ effort_all <- Second %>%
   group_by(Lon,Lat, month, year,flag, geartype) %>% 
   summarize(fishing_hours = sum(fishing_hours, na.rm = T), vessel_hours = sum(vessel_hours, na.rm = T))
 
-write.csv(effort_all, "Monthly_Pacific_2012-2016_fishingeffortflag_1.0.csv")   
+write.csv(effort_all, "GFW_Data/processed_data/pacific/Monthly_Pacific_2012-2016_fishingeffortmmsi_1.0.csv")   
 
 ### To subset Fishing_Data
 
